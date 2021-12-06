f151717321d897e0e276a46d50a43892c35fce24
Maram Mekni <maram.mekni@etudiant-isi.utm.tn>
2021-12-06 21:14:12 +0100

    TP5-RMIAssignment

--- src/Client.java
+++ src/Client.java
@@ -1,15 +1,33 @@
 import java.rmi.Naming;
+import java.util.Scanner;
 
 public class Client {
 
     public static void main(String[] args) {
         try {
             MyInterface service1 = (MyInterface) Naming.lookup("rmi://127.0.0.1:1250/server");
-            System.out.println(service1.reverse("RAGNAR"));
+            Scanner scanner=new Scanner(System.in);
+            System.out.println("Enter a string : ");
+            String ch=scanner.nextLine() ;
+            //System.out.println(service1.reverse(ch));
+            System.out.println("Input: "+ch);
+            System.out.println("Output : "+service1.reverse(ch));
+
+
             MyInterface service2 = (MyInterface) Naming.lookup("rmi://127.0.0.1:1250/server");
-            System.out.println(service2.minChar("Dexter"));
+            System.out.println("Enter a string : ");
+            String ch1=scanner.nextLine() ;
+            //System.out.println(service2.minChar("Dexter"));
+            System.out.println("Input: "+ch1);
+            System.out.println("Output : "+service2.minChar(ch1));
+
+
             MyInterface service3 = (MyInterface) Naming.lookup("rmi://127.0.0.1:1250/server");
-            System.out.println(service3.caseChanger("ThAnOs"));
+            System.out.println("Enter a string : ");
+            String ch2=scanner.nextLine() ;
+            //System.out.println(service3.caseChanger("ThAnOs"));
+            System.out.println("Input: "+ch2);
+            System.out.println("Output : "+service3.caseChanger(ch2));
 
 
 
